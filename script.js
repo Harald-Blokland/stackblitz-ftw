8b6b989634108cce9d8da294b3d900a1f5f79b16
Harald-Blokland <hablo81@gmail.com>
2025-04-06 15:35:59 +0200

    Api og Cat Fact

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 console.log('Hello!');
+[]
+()
+{}
+
+//DATATYPER//
+
+
+//Number
+
+let number = 23
+
+//String
+
+let name = "Harald"
+
+//Boleoon/
+
+let isTeaching = true
+
+//Errordata
+
+/**
+
+Undefined
+Null
+Nan
+
+*/
+
+//Array
+
+let shoppingList = [Shampo, middag];
+
+let numberList = [0, 3, 5];
+
+//Object
+
+console.log('dette er variabel for nummer:23' ) (number)
+
+let firstObject ={ firstName: `Harald`};
+
+let nrOne = '45'
+let nrTwo = '75'
+
+let sum = nrOne + nrTwo
+
+let surname = ('Blokland')
+
+let isLearning = ('true')
